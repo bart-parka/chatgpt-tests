426de9db6e812b7e192d2af6a16460c2803b81cb
Bart Parka <bartlomiej.parka@uk.ibm.com>
2023-01-28 15:46:12 +0000

    feat: refactor and cleanup

--- db.py
+++ db.py
@@ -8,3 +8,14 @@ def store_elongated_tweet(tweet):
 
 def retrieve_cached_tweets():
     return db.all()
+
+
+def tweet_dupe_check(tweet_id):
+    store = True
+    if len(db.all()) > 0:
+        for each in db.all():
+            if each["id"] == tweet_id:
+                store = False
+        return store
+    else:
+        return True
--- elongate.py
+++ elongate.py
@@ -6,16 +6,19 @@ import random
 import logging
 import argparse
 import sys
+import openai
 from datetime import datetime, timezone, timedelta
 from mdutils.mdutils import MdUtils
 from chatgpt_wrapper import ChatGPT
 from db import *
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Tweet ElonGator",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-l", "--log", help="Logging Level", default="INFO")
-parser.add_argument("-c", "--cache", help="Use Cached Tweets", default=True)
+parser.add_argument("-l", "--log", help="Logging Level", default="DEBUG")
+parser.add_argument("-c", "--cache", help="Use Cached Tweets", default=False)
 args = parser.parse_args()
 config = vars(args)
 print("Arguments Passed in: {}".format(config))
@@ -27,6 +30,38 @@ numeric_level = getattr(logging, args.log.upper(), None)
 if not isinstance(numeric_level, int):
     raise ValueError('Invalid log level: %s' % args.log)
 logging.basicConfig(filename='elongate.log', level=numeric_level)
+
+chatGPT = False
+GPT3 = True
+COMPARE = False
+
+users_list = [
+    # "ancientorigins",
+    "BillGates",
+    "NikkiSiapno",
+    "SamRamani2",
+    "oliverburkeman",
+    "GordonBrown",
+    # "HardcoreHistory",
+    # "engineers_feed",  # The first 10 are returned in order I think?
+    "sama",
+    "balajis",
+    "tobi",
+]
+
+questions = [
+    "Can you explain this tweet: ",
+    "Can you expand on this tweet: ",
+    "Can you give me background on this tweet: ",
+    "Can you give some context for this tweet: "
+]
+
+styles = {
+    "journalisitic": "Reply as though you are writing a newspaper article",
+    "scientific": "Reply acting as though you are a scientist",
+}
+
+# Twitter API
 
 
 def auth():
@@ -99,24 +134,23 @@ def generate_start_end_times(number_days):
 
     return start_time, end_time
 
+def retrieve_tweets(query, number_of_tweets):
+    bearer_token = auth()
+    headers = create_headers(bearer_token)
+    query_start_time, query_end_time = generate_start_end_times(1)
+    url, query_parameters = create_url(query, query_start_time, query_end_time, number_of_tweets)
+    json_response = connect_to_endpoint(url, headers, query_parameters)
+    return json_response
 
-def elongate_tweet(tweet_text):
-    questions = [
-        "Can you explain this tweet: ",
-        "Can you expand on this tweet: ",
-        "Can you give me background on this tweet: ",
-        "Can you give some context for this tweet: "
-    ]
 
-    styles = {
-        "journalisitic": "Reply as though you are writing a newspaper article",
-        "scientific": "Reply acting as though you are a scientist",
-    }
+# OpenAI APIs
 
+
+def elongate_tweet(text):
     # Removing any non-printable characters before feeding to ChatGPT, not sure if required
     printable = set(string.printable)
 
-    question_and_tweet = "{}".format(random.choice(questions)) + ''.join(filter(lambda x: x in printable, tweet_text))
+    question_and_tweet = "{}".format(random.choice(questions)) + ''.join(filter(lambda x: x in printable, text))
 
     logging.debug("ChatGPT question and tweet:\n" + question_and_tweet)
 
@@ -130,6 +164,57 @@ def elongate_tweet(tweet_text):
     logging.debug("ChatGPT output:\n" + output)
 
     return output
+
+
+def elongate_tweet_gpt3(text):
+
+    COMPLETIONS_MODEL = "text-davinci-003"
+    COMPLETIONS_API_PARAMS = {
+        "temperature": 1.0,  # 1 means as "creative" as possible
+        "max_tokens": 300,
+        "model": COMPLETIONS_MODEL,
+    }
+
+    # Removing any non-printable characters before feeding to ChatGPT, not sure if required
+    printable = set(string.printable)
+
+    question_and_tweet = "{}".format(random.choice(questions)) + ''.join(filter(lambda x: x in printable, text))
+
+    logging.debug("GPT-3 question and tweet:\n" + question_and_tweet)
+    response = openai.Completion.create(
+        prompt=question_and_tweet,
+        **COMPLETIONS_API_PARAMS,
+    )
+    output = response["choices"][0]["text"].strip(" \n")
+    print(output)  # This step seems to have fixed the ratelimiter, maybe need a backoff or timesleep
+    return output
+
+
+def elongate_tweets(tweets):
+    tweets_to_elongate = [t for t in tweets["data"] if tweet_dupe_check(t["id"])]
+
+    for tweet in tweets_to_elongate:
+        tweet_text = tweet["text"]
+        if chatGPT:
+            # Make sure you login first using `chatgpt install`
+            logging.info("Using ChatGPT Model")
+            chatgpt_response = elongate_tweet(tweet_text)
+            tweet["chatgpt_response"] = chatgpt_response
+        elif GPT3:
+            logging.info("Using GPT-3 Model")
+            gpt3_response = elongate_tweet_gpt3(tweet_text)
+            tweet["gpt3_response"] = gpt3_response
+        elif COMPARE:
+            logging.info("Using Both Models")
+            gpt3_response = elongate_tweet_gpt3(tweet_text)
+            chatgpt_response = elongate_tweet(tweet_text)
+            tweet["gpt3_response"] = gpt3_response
+            tweet["chatgpt_response"] = chatgpt_response
+        else:
+            logging.error("Need to set GPT3/chatGPT/COMPARE")
+        store_elongated_tweet(tweet)  # Store elongated tweet in local db
+
+# Markdown Generation
 
 
 def generate_blog_frontmatter():
@@ -157,12 +242,16 @@ def generate_blog_frontmatter():
     return inspect.cleandoc(front_matter)
 
 
-def generate_markdown_file(paragraphs):
-    now = datetime.now()
+def generate_markdown_file():
     md_file = MdUtils(file_name='ElonGator')
     md_file.write("This post was generated automatically.")
-    for para in paragraphs:
-        md_file.new_paragraph(para)
+    cached_tweets = retrieve_cached_tweets()
+    for tweet in cached_tweets:
+        author_id = tweet["author_id"]
+        tweet_id = tweet["id"]
+        md_file.new_paragraph(generate_embed_html(author_id, tweet_id) + "\n---\n")
+        md_file.new_paragraph(tweet.get("chatgpt_response", "ChatGPT Response not retrieved."))
+        md_file.new_paragraph(tweet.get("gpt3_response", "GPT-3 Response not retrieved."))
     md_file.write('  \n')
     md_file.create_md_file()
 
@@ -175,44 +264,12 @@ def prepend_blog_frontmatter(mdfile):
 
 
 if __name__ == '__main__':
-    text_blocks = []
-    if args.cache:
-        tweets = retrieve_cached_tweets()
-        for tweet in tweets:
-            author_id = tweet["author_id"]
-            tweet_id = tweet["id"]
-            text_blocks.append(generate_embed_html(author_id, tweet_id) + "\n---\n" + tweet["chatgpt_response"])
-    else:
-        # Make sure you login first using `chatgpt install`
+    if chatGPT or COMPARE:
         bot = ChatGPT()
 
-        users_list = [
-            "ancientorigins",
-            "engineers_feed",  # The first 10 are returned in order I think?
-            "sama",
-            "balajis",
-            "tobi",
-        ]
+    tweet_query = generate_query_string(users_list)
+    retrieved_tweets = retrieve_tweets(tweet_query, 10)
+    elongate_tweets(retrieved_tweets)
 
-        query = generate_query_string(users_list)
-        bearer_token = auth()
-        headers = create_headers(bearer_token)
-        query_start_time, query_end_time = generate_start_end_times(1)
-        url, query_parameters = create_url(query, query_start_time, query_end_time, 10)
-        json_response = connect_to_endpoint(url, headers, query_parameters)
-
-        for tweet in json_response["data"]:
-            author_id = tweet["author_id"]
-            tweet_id = tweet["id"]
-            tweet_text = tweet["text"]
-            chatgpt_response = elongate_tweet(tweet_text)
-            tweet["chatgpt_response"] = chatgpt_response
-            # This is wasteful as retrieves short tweets but can't work out how to filter by length of tweet in query
-            store_elongated_tweet(tweet)  # Store elongated tweet in local db
-            text_blocks.append(generate_embed_html(author_id, tweet_id) + "\n---\n" + chatgpt_response)
-
-    generate_markdown_file(text_blocks)
+    generate_markdown_file()
     prepend_blog_frontmatter('ElonGator.md')
-
-
-
